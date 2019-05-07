cd8f5af791942446235aa1cc3b404ff6e3df39b4
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2019-05-07 05:56:31 -0500

    Update video.jsx

--- video/video.jsx
+++ video/video.jsx
@@ -1,8 +1,25 @@
 const ReactDOMServer = require('react-dom/server');
 const React = require('react');
 
+const path = require('path');
+const fs = require('fs');
+
 const Video = props => <video src={props.name}></video>;
 
-const myFunc = name => ReactDOMServer.renderToString(<Video name={name} />)
+function RenderVideoReact(name) {
+    return ReactDOMServer.renderToString(<Video name={name} />;
+}
 
-module.exports = { myFunc };
+function RenderVideoFile(name, start, end) {
+    const path = path.resolve(__dirname, `${name}.mov`);
+    const file = fs.createReadStream(path, {start, end});
+
+    file.close();
+
+    return name;
+}
+
+module.exports = {
+    RenderVideoReact,
+    RenderVideoFile,
+};
